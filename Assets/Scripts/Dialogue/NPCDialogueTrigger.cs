c4637451105556d08eb67c8cfabda7ac6883332f
davidrk <david_knotten@hotmail.no>
2021-11-11 23:06:05 +0100

    Started working on a quest-management system
    Also implemented into dialogue system

--- Assets/Scripts/Dialogue/NPCDialogueTrigger.cs
+++ Assets/Scripts/Dialogue/NPCDialogueTrigger.cs
@@ -6,17 +6,34 @@ public class NPCDialogueTrigger : MonoBehaviour, IPlayerTriggable
 {
     [TextArea]
     public string[] sentences;
+    public QuestManager.Objective[] prerequisiteObjectives;
 
     public void OnPlayerTriggered(PlayerController player) {
-        //stop walk animaion
-        player.animator.SetBool("isMoving", false);
-        //continue dialog system
-        GameController.Instance.Dialog(true);
-        DialogueManager dialogueManager = DialogueManager.getInstance();
-        Queue<string> dialogueQueue = new Queue<string>();
-        foreach (string s in sentences) {
-            dialogueQueue.Enqueue(s);
+        bool ready = true;
+        foreach(QuestManager.Objective obj in prerequisiteObjectives) {
+            bool hit = false;
+            Debug.Log(obj.ToString());
+            foreach (QuestManager.Objective objective in QuestManager.Objective.GetValues(typeof(QuestManager.Objective))) {
+                Debug.Log(objective.ToString());
+                if (QuestManager.Instance.isComleted(obj)) hit = true;
+            }
+            if (!hit) ready = false;
         }
-        dialogueManager.startDialogue(dialogueQueue);
+
+        if (ready)
+        {
+            Debug.Log("Objectives fulfilled");
+            //stop walk animaion
+            player.animator.SetBool("isMoving", false);
+            //continue dialog system
+            GameController.Instance.Dialog(true);
+            DialogueManager dialogueManager = DialogueManager.getInstance();
+            Queue<string> dialogueQueue = new Queue<string>();
+            foreach (string s in sentences)
+            {
+                dialogueQueue.Enqueue(s);
+            }
+            dialogueManager.startDialogue(dialogueQueue);
+        }
     }
 }
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // To add objectives, add an entry to the enum
+    public enum Objective {TTFirstMan, FindBone, EnterHouse, None}
+
+    private Dictionary<Objective, bool> progress;
+    public static QuestManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Start()
+    {
+        Instance = new QuestManager();
+        Instance.setupObjectives();
+    }
+
+    public bool isComleted(Objective objective) {
+        return progress[objective];
+    }
+
+    private void setupObjectives() {
+        progress = new Dictionary<Objective, bool>();
+        foreach (Objective obj in (Objective[]) Objective.GetValues(typeof(Objective))) {
+            progress.Add(obj, false);
+        }
+        progress[Objective.None] = true;
+    }
+
+
+}
