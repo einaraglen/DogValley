b9437e8c1347a56e9969f4eefc2a9e213a7b9606
davidrk <david_knotten@hotmail.no>
2021-11-12 12:29:37 +0100

    Added prerequisite objectives to QTrigger

--- Assets/Scripts/QuestTrigger.cs
+++ Assets/Scripts/QuestTrigger.cs
@@ -6,7 +6,16 @@ public class QuestTrigger : MonoBehaviour, IPlayerTriggable
 {
 
     public QuestManager.Objective objective;
+    public QuestManager.Objective[] prerequisiteObjectives;
     public void OnPlayerTriggered(PlayerController player) {
+        bool ready = true;
+        foreach(QuestManager.Objective obj in prerequisiteObjectives)
+        {
+            if (!QuestManager.Instance.isComleted(obj)) ready = false;
+        }
+
+        if (!ready) return;
+
         QuestManager.Instance.completeObjective(objective);
     }
 }
