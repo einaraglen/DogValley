1050a2796e7b7e4fa2dbc3bff535d21d1972737c
Einar <einar.aglen99@gmail.com>
2021-11-22 14:55:48 +0100

    Small fixes

--- Assets/Scripts/ObjectiveObstacle.cs
+++ Assets/Scripts/ObjectiveObstacle.cs
@@ -2,32 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectiveObstacle : MonoBehaviour, ObjectiveListener
-{
+public class ObjectiveObstacle : MonoBehaviour, ObjectiveListener {
     public QuestManager.Objective killObjective = QuestManager.Objective.None;
     public bool activeAfter = false;
 
-    private void Start()
-    {
-        if (!QuestManager.Instance.isComleted(killObjective))
-        {
+    private void Start() {
+        if (!QuestManager.Instance.isComleted(killObjective)) {
             QuestManager.Instance.listenTo(killObjective, this);
         }
     }
 
-    private void OnLevelWasLoaded()
-    {
-        if (QuestManager.Instance.isComleted(killObjective))
-        {
+    private void OnLevelWasLoaded() {
+        if (QuestManager.Instance.isComleted(killObjective)) {
             this.gameObject.SetActive(activeAfter);
         }
     }
 
-    public void objectiveCompleted(QuestManager.Objective listenedObj)
-    {
+    public void objectiveCompleted(QuestManager.Objective listenedObj) {
         Debug.Log("Listened");
-        if (listenedObj == killObjective)
-        {
+        if (listenedObj == killObjective) {
             //check if obstacle has sprite render and activeAfter is true
             if (this.GetComponent<SpriteRenderer>() != null && activeAfter) {
                 this.GetComponent<SpriteRenderer>().enabled = true;
@@ -37,8 +30,7 @@ public class ObjectiveObstacle : MonoBehaviour, ObjectiveListener
         }
     }
 
-    private void OnDestroy()
-    {
+    private void OnDestroy() {
         QuestManager.Instance.stopListening(killObjective, this);
     }
 }
