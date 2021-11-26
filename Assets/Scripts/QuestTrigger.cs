e73b30a0ce60c678c3d75d1e7353442c8f0f4861
Einar <einar.aglen99@gmail.com>
2021-11-26 19:08:06 +0100

    Added new characters

--- Assets/Scripts/QuestTrigger.cs
+++ Assets/Scripts/QuestTrigger.cs
@@ -1,16 +1,14 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestTrigger : MonoBehaviour, IPlayerTriggable
-{
+public class QuestTrigger : MonoBehaviour, IPlayerTriggable {
 
     public QuestManager.Objective objective;
     public QuestManager.Objective[] prerequisiteObjectives;
     public void OnPlayerTriggered(PlayerController player) {
         bool ready = true;
-        foreach(QuestManager.Objective obj in prerequisiteObjectives)
-        {
+        foreach (QuestManager.Objective obj in prerequisiteObjectives) {
             if (!QuestManager.Instance.isComleted(obj)) ready = false;
         }
 
